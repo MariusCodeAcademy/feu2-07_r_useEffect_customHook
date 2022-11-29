35ed71ff94d666928913ac3f27364ab9f6f74fe0
Marius <marius@example.com>
2022-11-29 08:44:52 +0200

    Products

---
+++ src/components/Products.jsx
@@ -0,0 +1,15 @@
+import './Products.css';
+
+function Products(props) {
+  return (
+    <div>
+      <h2>Products</h2>
+      <ul className='unlisted grid--pr'>
+        <li className='singleProduct'>one</li>
+        <li className='singleProduct'>one</li>
+        <li className='singleProduct'>one</li>
+      </ul>
+    </div>
+  );
+}
+export default Products;
