1fe8fb92f1dd56cee91db3f98eb0f13ebe008dc7
Marius <marius@example.com>
2022-11-29 09:00:59 +0200

    rest post

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,24 @@
+import { useState, useEffect } from 'react';
 import './Products.css';
 
 function Products(props) {
+  // komponentas kuris parsisiuncia duomenis
+  // 1. sugeneruojam komponenta su tusciu masyvu be duomenu
+  const [mainProductsArray, setMainProductsArray] = useState([]);
+  // 2. tik sugeneravus useEffecte parsiunciam duomenis
+  useEffect(() => {
+    getProducts();
+  }, []);
+  // 3. parsiuntus atnaujinam tuscia state masyva su gautais duomenimis
+  // react pats nubraizo pakeitimus html
+
+  async function getProducts() {
+    const url = 'https://golden-whispering-show.glitch.me';
+    const resp = await fetch(url);
+    const dataInJs = await resp.json();
+    console.log('dataInJs ===', dataInJs);
+  }
+
   return (
     <div>
       <h2>Products</h2>
