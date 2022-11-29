63eb1130a7b119c8bcf691b9e2254aafd1420ada
Marius <marius@example.com>
2022-11-29 10:24:55 +0200

    delete trigger

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -6,13 +6,18 @@
 
 function SingleProduct(props) {
   // props.onDelete === productDeleteHandler (esanciame Products cmp)
+
+  function deleteTrigger() {
+    props.onDelete(props.id);
+  }
+
   return (
     <li className='singleProduct'>
       <img src={props.image} alt={props.children} />
       <h3>{props.children}</h3>
       <p className='singleP__price'>$ {props.price}</p>
       {/* pasiiimti id is props ir paduoti i onDelete */}
-      <button onClick={() => props.onDelete(props.id)}>delete ⁉ </button>
+      <button onClick={deleteTrigger}>delete ⁉ </button>
     </li>
   );
 }
