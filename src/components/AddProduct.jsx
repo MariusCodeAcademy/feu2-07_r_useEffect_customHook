897b067f189c7de777be719002d6b1cef395dfdc
Marius <marius@example.com>
2022-11-29 11:23:13 +0200

    two states inputs

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,17 +1,45 @@
+import { useState } from 'react';
 function AddProduct(props) {
+  // sukurti handleSubmit funkcija
+  // paleisti ja patekiant forma
+  // sustabdyti perkrovima
+
+  const [titleValue, setTitleValue] = useState('');
+
+  function titleInputHadler(event) {
+    setTitleValue(event.target.value);
+  }
+
+  const [imageUrl, setImageUrl] = useState('');
+
+  function imageInputHandler(e) {
+    setImageUrl(e.target.value);
+  }
+
   return (
     <fieldset>
       <legend>AddProduct</legend>
       <form>
-        <input type='text' placeholder='title' />
-        <input type='text' placeholder='image url' />
+        <input
+          type='text'
+          onChange={titleInputHadler}
+          value={titleValue}
+          placeholder='title'
+        />
+        <input
+          onChange={imageInputHandler}
+          value={imageUrl}
+          type='text'
+          placeholder='image url'
+        />
         <input type='number' step={0.01} placeholder='price' />
         <button type='submit'>Create</button>
       </form>
 
       <p>
-        Title: <br />
-        ImageUrl: <br />
+        Title: {titleValue}
+        <br />
+        ImageUrl: {imageUrl} <br />
         Price: <br />
       </p>
     </fieldset>
